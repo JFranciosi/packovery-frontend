55df854c1957c105472d5b28bb7405261b5249a7
JFranciosi <luigigigio.jf@gmail.com>
2026-01-15 14:40:45 +0100

    Refactor: Add title in routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent }
+    {
+        title: 'Login',
+        path: '',
+        component: LoginComponent
+    }
 ];
